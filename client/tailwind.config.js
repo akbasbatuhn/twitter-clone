a9f890ba783588bfd9f289f5f1f2a3ae5186e6fd
Batuhan Akbaş <batuhanakbas011@gmail.com>
2023-04-14 20:19:02 +0300

    add twitter colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    base: "rgb(29 155 240)",
+                    dark: "rgb(25 140 216)",
+                    light: "rgb(150 197 236)",
+                },
+                gray: {
+                    dark: "rgb(83, 100, 113)",
+                    light: "#AAB8C2",
+                    extraLight: "#E1E8ED",
+                    lightest: "F5F8FA",
+                },
+                black: {
+                    primary: "rgb(15 20 26)",
+                },
+            },
+        },
     },
     plugins: [],
 };
