7ac354fc752c917d9f347ae84f6bd6ab29ae0fba
Batuhan Akbaş <batuhanakbas011@gmail.com>
2023-04-16 16:50:27 +0300

    change colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,13 +7,13 @@ export default {
                 primary: {
                     base: "rgb(29 155 240)",
                     dark: "rgb(25 140 216)",
-                    light: "rgb(150 197 236)",
+                    light: "rgb(208, 230, 249)",
                 },
                 gray: {
                     dark: "rgb(83, 100, 113)",
                     light: "#AAB8C2",
                     extraLight: "#E1E8ED",
-                    lightest: "F5F8FA",
+                    lightest: "#F5F8FA",
                 },
                 black: {
                     primary: "rgb(15 20 26)",
