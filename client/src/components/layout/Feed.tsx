fd661230c27f896bc0aa375df6e56385f4f158ce
Batuhan Akbaş <batuhanakbas011@gmail.com>
2023-06-02 17:18:16 +0300

    Add: bg blur to home page header

--- client/src/components/layout/Feed.tsx
+++ client/src/components/layout/Feed.tsx
@@ -6,7 +6,7 @@ const Feed = () => {
         <main className="flex flex-col">
             <div
                 className="sticky z-10 top-0 flex p-4 border-t border-b 
-                border-gray-extraLight bg-white"
+                border-gray-extraLight bg-white bg-opacity-70 backdrop-blur-sm"
             >
                 <header>
                     <span className="font-bold text-xl text-gray-900 bg-transparent">
