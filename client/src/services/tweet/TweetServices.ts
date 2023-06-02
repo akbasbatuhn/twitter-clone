b94892cfa009d770be4ecf4fae3a296175d0a65c
Batuhan Akbaş <batuhanakbas011@gmail.com>
2023-06-02 17:07:28 +0300

    delete: unnecessary comments

--- client/src/services/tweet/TweetServices.ts
+++ client/src/services/tweet/TweetServices.ts
@@ -12,14 +12,6 @@ export const getAllTweets = async (userId: number) => {
     const userToken = getUserTokenFromLocalStorage();
 
     return GetWithAuth(url, userToken!);
-
-    // return await fetch("http://localhost:8080/tweets", {
-    //     method: "GET",
-    //     headers: {
-    //         "Content-Type": "application/json",
-    //         Authorization: userToken,
-    //     },
-    // });
 };
 
 export const getAllTweetsByUserId = (userId: number) => {
@@ -27,14 +19,6 @@ export const getAllTweetsByUserId = (userId: number) => {
     const userToken = getUserTokenFromLocalStorage();
 
     return GetWithAuth(url, userToken!);
-
-    // return await fetch(`http://localhost:8080/tweets?userId=${userId}`, {
-    //     method: "GET",
-    //     headers: {
-    //         "Content-Type": "application/json",
-    //         Authorization: userToken,
-    //     },
-    // });
 };
 
 export const getSingleTweetByTweetId = (id: number) => {
@@ -42,14 +26,6 @@ export const getSingleTweetByTweetId = (id: number) => {
     const userToken = getUserTokenFromLocalStorage();
 
     return GetWithAuth(url, userToken!);
-
-    // return await fetch(`http://localhost:8080/tweets/${id}`, {
-    //     method: "GET",
-    //     headers: {
-    //         "Content-Type": "application/json",
-    //         Authorization: userToken,
-    //     },
-    // });
 };
 
 export type CreateTweetRequest = {
@@ -61,24 +37,8 @@ export type CreateTweetRequest = {
 export const postTweet = (tweetData: CreateTweetRequest) => {
     const url = "http://localhost:8080/tweets";
     const userToken = getUserTokenFromLocalStorage();
-    // console.log(tweetData);
 
     return PostWithAuth(url, tweetData, userToken!);
-
-    // const res = await fetch("http://localhost:8080/tweets", {
-    //     method: "POST",
-    //     headers: {
-    //         "Content-Type": "application/json",
-    //         Authorization: userToken,
-    //     },
-    //     body: JSON.stringify({
-    //         userId: userId,
-    //         text: text,
-    //     }),
-    // });
-
-    // const data = await res.json();
-    // return data;
 };
 
 export const getTweetsLikedByGivenUser = (userId: number) => {
